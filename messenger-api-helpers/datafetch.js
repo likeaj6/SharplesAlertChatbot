d9fc7dabfc7d4bfcb890741767af3c195bb4fb22
Jason Jin <jasonjin@Jasons-MacBook-Pro.local>
2017-11-12 23:54:22 -0500

    promises

--- messenger-api-helpers/datafetch.js
+++ messenger-api-helpers/datafetch.js
@@ -1,24 +1,35 @@
-import request from 'request';
+import rp from 'request-promises';
 
 import UserStore from '../stores/user-store';
 
 
 const fetchUserName = (userPSID) => {
     const usersPublicProfile = 'https://graph.facebook.com/v2.6/' + userPSID + '?fields=first_name,last_name&access_token=&access_token=' + process.env.PAGE_ACCESS_TOKEN;
-    request({
+    var options = {
         url: usersPublicProfile,
         json: true // parse
-    }, function (error, response, body) {
-        return new Promise((resolve, reject) => {
-                if (!error && response.statusCode === 200) {
-                    setUserName(userPSID, body.first_name, body.last_name);
-                    console.log(body)
-                    resolve(body.first_name);
-                } else {
-                    reject(error);
-                }
-              });
-        });
+    }
+
+    return rp(options).then(function (result) {
+        if (result.statusCode === 200) {
+            setUserName(userPSID, body.first_name, body.last_name);
+            return body.first_name
+        }
+    }).catch(function (error) {
+
+    });
+
+    // , function (error, response, body) {
+    //     return new Promise((resolve, reject) => {
+    //             if (!error && response.statusCode === 200) {
+    //                 setUserName(userPSID, body.first_name, body.last_name);
+    //                 console.log(body)
+    //                 resolve(body.first_name);
+    //             } else {
+    //                 reject(error);
+    //             }
+    //           });
+    //     });
 };
 
 const setUserName = (id, firstName, lastName) => {
